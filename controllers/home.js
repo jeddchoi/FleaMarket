f5e13c043d31a1ddd86aad2c8dc4b91aa6dbe745
Gwangyoung Choi <jed.gy.choi@gmail.com>
2019-12-10 20:43:40 +0900

    all products and category

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,8 @@ const Product = require('../models/Product')
 module.exports.index = (req, res) => {
   let queryData = req.query
 
-  Product.find({buyer: null}).populate('category').then((products) => {
+  // find all registered products
+  Product.find({status:'Registered'}).populate('category').then((products) => {
     if (queryData.query) {
       products = products.filter(p => p.name.toLowerCase().includes(queryData.query))
     }
