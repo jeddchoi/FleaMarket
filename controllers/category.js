ac55c0b45f7e4067000f16fc491169204e1c7b24
Gwangyoung Choi <jed.gy.choi@gmail.com>
2019-12-13 19:14:25 +0900

    merge

--- controllers/category.js
+++ controllers/category.js
@@ -1,12 +1,7 @@
 let Category = require('../models/Category')
 
 module.exports.index = (req, res) => {
-  Category.find()
-    .then((categories) => {
-      if (!categories) {
-        res.sendStatus(404)
-        return
-      }
+  Category.find().then((categories) => {
       Category.findOne().populate('products').then((selectedCategory) => {
         res.render('category/index', {
           categories: categories,
